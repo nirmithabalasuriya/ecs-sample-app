60daa516bb6edc56bfc0ea4217cbdd40df5eab9e
Nirmitha-UI <nirmitha@derana.lk>
2025-10-07 12:28:08 +0530

    main

---
+++ frontend/app/page.js
@@ -0,0 +1,8 @@
+// frontend/app/page.js
+export default function Page() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Hello from Next.js Frontend 🚀</h1>
+    </div>
+  );
+}
